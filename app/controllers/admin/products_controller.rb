85ff6335a08731e5f2c2e7a97c2ebb0342255d87
Leon-Mbegera <mbegeraleon89@gmail.com>
2025-01-25 17:26:06 +0300

    define product create action

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -7,4 +7,21 @@ class Admin::ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+
+  def create
+    product = Product.new(product_params)
+
+    if product.save
+      redirect_to admin_products_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :quantity, :description)
+  end
 end
