7b127016ef648fd8b1ebb4f9489763da885e8dd3
Leon-Mbegera <mbegeraleon89@gmail.com>
2025-01-26 10:31:35 +0300

    define product show action

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -18,6 +18,10 @@ class Admin::ProductsController < ApplicationController
     end
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
   def edit
     @product = Product.find(params[:id])
   end
