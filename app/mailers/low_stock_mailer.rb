7f842502dc93d64bf68f7fd96dd8317be4ad5158
Leon-Mbegera <mbegeraleon89@gmail.com>
2025-01-27 17:04:23 +0300

    define the mailer action

--- app/mailers/low_stock_mailer.rb
+++ app/mailers/low_stock_mailer.rb
@@ -1,2 +1,11 @@
 class LowStockMailer < ApplicationMailer
+  default from: 'no-reply@example.com'
+
+  def low_stock_notification(sale)
+    @product = sale.product
+    mail(
+      to: 'recipient@example.com', # Replace with recipient's email
+      subject: "#{@product.name} Stock Level."
+    )
+  end
 end
