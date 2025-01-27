66105d9202d57a56bbddc8e0bbc60d6b519c81d9
Leon-Mbegera <mbegeraleon89@gmail.com>
2025-01-27 15:01:02 +0300

    annotate products model and affiliate files

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,13 +2,14 @@
 #
 # Table name: products
 #
-#  id          :integer          not null, primary key
-#  description :string
-#  name        :string
-#  price       :string
-#  quantity    :integer
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
+#  id              :integer          not null, primary key
+#  description     :string
+#  low_stock_level :integer          default(0), not null
+#  name            :string
+#  price           :string
+#  quantity        :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
 #
 class Product < ApplicationRecord
   has_one_attached :image
