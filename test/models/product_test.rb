671348dae773595b38bdcb17bed54e2495864569
Leon-Mbegera <mbegeraleon89@gmail.com>
2025-01-27 19:56:25 +0300

    product and sale unit tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -14,7 +14,28 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @product_one = products(:product_one)
+    @product_two = products(:product_two)
+  end
+
+  test "valid product" do
+    assert @product_one.valid?
+  end
+
+  test "invalid without name" do
+    @product_one.name = nil
+    assert_not @product_one.valid?, "Product is valid without a name"
+  end
+
+  test "low_stock_level must be greater than or equal to 0" do
+    @product_one.low_stock_level = -1
+    assert_not @product_one.valid?, "Product is valid with a negative low_stock_level"
+  end
+
+  test "has many sales" do
+    assert_respond_to @product_one, :sales
+    assert_equal 1, @product_one.sales.size
+  end
 end
+
--- test/models/sale_test.rb
+++ test/models/sale_test.rb
@@ -20,7 +20,34 @@
 require "test_helper"
 
 class SaleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @sale_one = sales(:sale_one)
+    @sale_two = sales(:sale_two)
+    @product_one = products(:product_one)
+  end
+
+  test "valid sale" do
+    assert @sale_one.valid?
+  end
+
+  test "quantity must be greater than 0" do
+    @sale_one.quantity = 0
+    assert_not @sale_one.valid?, "Sale is valid with a quantity of 0"
+  end
+
+  test "quantity greater than product stock is invalid" do
+    @sale_one.quantity = @product_one.quantity + 1
+    assert_not @sale_one.valid?, "Sale is valid with a quantity exceeding product stock"
+  end
+
+  test "product quantity is reduced after sale creation" do
+    initial_quantity = @product_one.quantity
+    new_sale = Sale.create!(product: @product_one, quantity: 5)
+    assert_equal initial_quantity - 5, @product_one.reload.quantity
+  end
+
+  test "date formats correctly" do
+    assert_equal @sale_one.created_at.strftime("%a %d-%m-%Y at %I:%M%p"), @sale_one.date
+  end
 end
+
